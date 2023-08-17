cb8aad504b12ac32893e3667918f4381d95455df
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-17 13:41:25 -0500

    add

--- youtube_channel.py
+++ youtube_channel.py
@@ -13,13 +13,35 @@ if __name__ == '__main__':
         # Extraer el ID del canal de la URL
         channel_id = sys.argv[1] # ejem. @exitosape en 'https://www.youtube.com/@exitosape/videos'
 
-        # Realizar la solicitud para obtener la metadata del canal
-        response = youtube.channels().list(
-            part='snippet,contentDetails,statistics',
-            id=channel_id
-        ).execute()
+        # Inicializar la lista para almacenar los videos
+        videos = []
 
-        # Extraer la información relevante de la respuesta
-        print("################################3")
-        print(response)
-        print("################################3")
+        # Definir el token de paginación inicial
+        next_page_token = None
+
+        # Ciclo para obtener todos los videos del canal
+        while True:
+            # Realizar la solicitud para obtener la lista de videos del canal
+            response = youtube.search().list(
+                part='id',
+                channelId=channel_id,
+                maxResults=50,  # Puedes ajustar la cantidad de videos por solicitud
+                order='date',  # Ordenar por fecha
+                pageToken=next_page_token
+            ).execute()
+
+            # Agregar los IDs de video a la lista
+            for item in response['items']:
+                videos.append(item['id']['videoId'])
+
+            # Verificar si hay más páginas de resultados
+            next_page_token = response.get('nextPageToken')
+            if not next_page_token:
+                break
+
+        # Imprimir la lista de IDs de video
+        print(f'Total de videos encontrados: {len(videos)}')
+        print('Lista de IDs de video:')
+        for video_id in videos:
+            print(video_id)
+
