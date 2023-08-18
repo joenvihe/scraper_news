96d74893a53c95344b276660f440e0afe13980a4
Jorge Enrique Vicente Hernández <enrique@vicenteh.com>
2023-08-17 23:55:16 -0500

    and

--- app_date.py
+++ app_date.py
@@ -100,7 +100,7 @@ def get_noticias():
     query = """
     SELECT DISTINCT periodico,_id,contenido  
     FROM public.noticias 
-    WHERE contenido IS NOT NULL and noticias_json IS NOT NULL AND
+    WHERE contenido IS NOT NULL and noticias_json IS NOT NULL
     AND EXTRACT(YEAR FROM TO_DATE(display_date, 'YYYY-MM-dd')) = 2023
     AND EXTRACT(MONTH FROM TO_DATE(display_date, 'YYYY-MM-dd')) = 8
     """
